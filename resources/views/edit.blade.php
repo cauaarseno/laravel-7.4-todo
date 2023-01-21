e25a124fdb7144ba5c76d0e593b3a87f980a6c08
Cauã Arseno <cauaarseno61@gmail.com>
2023-01-20 21:38:57 -0300

    edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-@section('title', '| Dashboard')
-
 @section('content')
     <a href="{{ route('logout') }}" class="position-absolute top-0 end-0 link-secondary p-3">
         <i class="bi bi-box-arrow-right fs-3"></i>
@@ -9,7 +7,6 @@
     <div class="min-vh-100 d-flex justify-content-center align-items-center">
         <div class="shadow-lg p-3 bg-dark text-white rounded p-5" style="width: 850px; min-height: 300px;">
             <h1>Editar Tarefa</h1>
-            <small class="text-info">{{ $todo->where('is_complete', 0)->count() }} ativas</small>
             <form action="/todos/{{$todo->id}}" method="post" class="mt-3">
                 @csrf
                 @method('PUT')
@@ -17,11 +14,13 @@
                     <div class="input-group-text bg-white p-0">
                         <input type="color" class="form-control form-control-color border-0" name="color" title="Escolha uma cor">
                     </div>
-                    <input type="text" class="form-control" name="title" value="{{ $todo->id }}" required>
+                    <input type="text" class="form-control" name="title" value="{{$todo->title}}" required>
                     <button type="submit" class="btn btn-secondary">
                         <i class="bi bi-plus fs-4"></i>
                     </button>
                 </div>
             </form>
+        </div>
+    </div>
 
 @endsection
